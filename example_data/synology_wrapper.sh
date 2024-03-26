2ec1dff8cdc01171741768452cbfd6d7f58764c0
Salvoxia <salvoxia@blindfish.info>
2024-03-26 20:30:03 +0100

    Synology Wrapper Updated for better compatiblity

--- example_data/synology_wrapper.sh
+++ example_data/synology_wrapper.sh
@@ -1,6 +1,15 @@
+#!/usr/bin/env bash
+# Make sure to add the task for initializing optware as a prerequisite!
+# Add this wrapper as a "Triggered Task" -> "Custom Script" 
+# /bin/bash ./wrapper.sh <exporterArguments>
+
 # Add optware to PATH to ensure the correct smartctl version is found
 export PATH=/opt/sbin:/opt/bin:$PATH
+# Prevent error
+# ERROR: ld.so: object 'openhook.so' from LD_PRELOAD cannot be preloaded (cannot open shared object file): ignored.
+# when getting started from Task Planner
+export LD_PRELOAD=/usr/lib/openhook.so:/usr/lib/libapparmor.so.1
 # Locate current script, we assume the exporter is sitting in the same folder
 BASEDIR=$(dirname "$0")
 # Run exporter, passing all arguments passed to the wrapper
-bash $BASEDIR/graphite_smart_exporter.sh "$@"
+$BASEDIR/graphite_smart_exporter.sh $@
